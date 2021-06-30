4409fe6ee596002ea4ee38c7bcb3a9e0c228f8e0
Jared Burkamper <jaredburkamper@gmail.com>
2021-06-30 13:58:52 -0500

    Tidied up some code and added package-lock to .gitignore

--- src/dynamoDB/dynamoDB.ts
+++ src/dynamoDB/dynamoDB.ts
@@ -5,26 +5,31 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 const REGION = process.env.AWS_DEFAULT_REGION; //e.g. "us-east-1"
 // Create an Amazon DynamoDB service client object.
 
-const ddbClient = new DynamoDBClient({ region: REGION , 
-    credentials: {accessKeyId: process.env.AWS_ACCESS_KEY || "", secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ""},},);
+const ddbClient = new DynamoDBClient({ 
+    region: REGION, 
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY || "", 
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ""
+    }
+});
 
-    const marshallOptions = {
-        // Whether to automatically convert empty strings, blobs, and sets to `null`.
-        convertEmptyValues: false, // false, by default.
-        // Whether to remove undefined values while marshalling.
-        removeUndefinedValues: false, // false, by default.
-        // Whether to convert typeof object to map attribute.
-        convertClassInstanceToMap: false, // false, by default.
-    };
-    
-    const unmarshallOptions = {
-        // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
-        wrapNumbers: false, // false, by default.
-    };
-    
-    const translateConfig = { marshallOptions, unmarshallOptions };
-    
-    // Create the DynamoDB Document client.
-    const ddbDocClient = DynamoDBDocumentClient.from(ddbClient, translateConfig);
-    
-    export { ddbDocClient };
+const marshallOptions = {
+    // Whether to automatically convert empty strings, blobs, and sets to `null`.
+    convertEmptyValues: false, // false, by default.
+    // Whether to remove undefined values while marshalling.
+    removeUndefinedValues: false, // false, by default.
+    // Whether to convert typeof object to map attribute.
+    convertClassInstanceToMap: false, // false, by default.
+};
+
+const unmarshallOptions = {
+    // Whether to return numbers as a string instead of converting them to numbers.
+    wrapNumbers: false, // false, by default.
+};
+
+const translateConfig = { marshallOptions, unmarshallOptions };
+
+// Create the DynamoDB Document client.
+const ddbDocClient = DynamoDBDocumentClient.from(ddbClient, translateConfig);
+
+export { ddbDocClient };
