7bfa84e343b6ebe8bca82aa9c2ad86808f3e1835
Kai <kaiyip.ho23@gmail.com>
2021-07-07 14:25:49 -0400

    jest dynalite config file

--- jest-dynalite-config.js
+++ jest-dynalite-config.js
@@ -1,14 +1,47 @@
 export default {
   tables: [
     {
-      TableName: "table",
-      KeySchema: [{ AttributeName: "id", KeyType: "HASH" }],
-      AttributeDefinitions: [{ AttributeName: "id", AttributeType: "S" }],
+      TableName: "bg-posts",
+      KeySchema: [{ AttributeName: "userName", KeyType: "HASH" }],
+      AttributeDefinitions: [
+        { AttributeName: "userName", AttributeType: "S" },
+        { AttributeName: "postTime", AttributeType: "N" }
+    ],
       ProvisionedThroughput: {
         ReadCapacityUnits: 1,
         WriteCapacityUnits: 1,
       },
     },
+    {
+      TableName: "bg-users",
+      KeySchema: [{ AttributeName: "userName", KeyType: "HASH" }],
+      AttributeDefinitions: [
+              { AttributeName: "userName", AttributeType: "S" },
+              { AttributeName: "loginToken", AttributeType: "S" }
+            ],
+      GlobalSecondaryIndexes: [
+        {
+          IndexName: "loginToken-index",
+          KeySchema: [
+            {
+              AttributeName: "loginToken",
+              KeyType: "HASH"
+            }
+          ],
+          Projection: {
+            ProjectionType: "ALL"
+          },
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+          },
+        }
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1,
+      },
+    }
   ],
   basePort: 8000,
 };
