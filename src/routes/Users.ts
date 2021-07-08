030818c8ef5843c1b8afa5df1977e148fc21b839
Jared Burkamper <jaredburkamper@gmail.com>
2021-07-07 19:56:11 -0500

    Users route testing done

--- src/routes/Users.ts
+++ src/routes/Users.ts
@@ -9,9 +9,10 @@ export async function createUser(req: Request, res: Response){
     try {
         const {user} = req.body;
         const target = await userDao.createUser(user)
-        res.status(200).json(target);
+        if (target.success) res.status(200).json(target);
+        else res.status(400).json(target);
     } catch(error){
-        res.status(500).json({err:error})
+        res.status(400).json({err:error})
     }
 }
 
@@ -19,10 +20,10 @@ export async function getUser(req: Request, res: Response){
     try {
         const {userName} = req.params;
         const user = await userDao.getUser(userName);
-        res.status(200).json(user);
+        if (user.success) res.status(200).json(user);
+        else res.status(400).json(user);
     } catch(error){
-        console.error(error);
-        res.status(500).json({err:"something went wrong"})
+        res.status(400).json({err:"something went wrong"})
     }
 } 
 
@@ -30,22 +31,21 @@ export async function authenticate(req: Request, res: Response){
     try {
         const {userName, password} = req.body;
         const user = await userDao.authenticate(userName, password);
-        res.status(200).json(user);
+        if (user.success) res.status(200).json(user);
+        else res.status(400).json(user)
     } catch(error){
-        console.error(error);
-        res.status(500).json({err:"something went wrong"})
+        res.status(400).json({err:"something went wrong"})
     }
 }
 
 export async function updateUser(req: Request, res: Response){
     try {
-        console.log(req.body);
         const {user, loginToken} = req.body;
         const target = await userDao.updateUser(loginToken, new User(user))
-        res.status(200).json(target);
+        if (target.success) res.status(200).json(target);
+        else res.status(400).json(target);
     } catch(error){
-        console.log(error);
-        res.status(500).json({err:error})
+        res.status(400).json({err:error})
     }
 }
 
@@ -53,9 +53,10 @@ export async function deleteUser(req: Request, res: Response){
     try {
         const {loginToken} = req.params
         const profile = await userDao.deleteUser(loginToken);
-        res.status(200).json(profile);
+        if (profile.success) res.status(200).json(profile);
+        else res.status(400).json(profile);
     } catch(error){
-        res.status(500).json({err:"something went wrong"})
+        res.status(400).json({err:"something went wrong"})
     }
 }
 
--- src/test/routes/user.test.ts
+++ src/test/routes/user.test.ts
@@ -1,83 +1,158 @@
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/unbound-method */
 import { getMockReq, getMockRes } from '@jest-mock/express';
-import { createUser, getUser, authenticate, updateUser, deleteUser} from '../../routes/Users';
+import { createUser, getUser, authenticate, updateUser, deleteUser } from '../../routes/Users';
+import { ddbDocClient as dynamo } from '../../dynamoDB/dynamoDB';
+import { PutCommand } from '@aws-sdk/lib-dynamodb';
 
-const req = getMockReq({
-  params: {userName:"Triangle", displayName: "The all seeing eye", loginToken: "123456", email: "theOne@gmail.com", password:"hashedPassword"},
-  
-  body: { userName:"lady", displayName: "eyeball", loginToken: "56789", email: "grove@gmail.com", password:"hashedPassword123"}
+const testUserInDB = {
+  userName: "NewUser",
+  password: "$2b$10$dYwdzHITigXLSpS1/LRBcuSyN7xA7ap1Hwr7IHr/FkAx3nbOEy8by",
+  email: "not@real.com",
+  profileImg: "http://www.fake.com/img.png",
+  displayName: "The Newest User",
+  loginToken: "760a950e-9f7d-4e2b-a07a-803b9ef7f4c7"
+}
+
+const testUser = {
+  userName: "NewUser",
+  password: "hashed",
+  email: "not@real.com",
+  profileImg: "http://www.fake.com/img.png",
+  displayName: "The Newest User"
+}
+
+beforeEach(async () => {
+  await dynamo.send(new PutCommand({
+    TableName: "bg-users",
+    Item: testUserInDB
+  }));
 });
 
-it('should create a new user with the params', async() => {
-  const {res, next, mockClear } = getMockRes();
-  mockClear();
-  await createUser(req, res);
-  expect(res.status).toHaveBeenCalledWith(200);
+afterAll(() => {
+  dynamo.destroy();
+});
 
-  it('should return an error if there are no params', async() => {
+describe('Create user', () => {
+  it('should create a new user with the params', async () => {
+    const newUser = {
+      ...testUser,
+      userName: "bob"
+    }
+    const req = getMockReq({
+      body: {
+        user: newUser
+      }
+    });
+    const { res, next, mockClear } = getMockRes();
+    mockClear();
+    await createUser(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+
+  it('should return an error if there are no params', async () => {
     const req = getMockReq();
-    const {res, next, mockClear } = getMockRes();
+    const { res, next, mockClear } = getMockRes();
     mockClear();
     await createUser(req, res);
     expect(res.status).toHaveBeenCalledWith(400);
-  })
-})
+  });
+});
 
-it('should get a user with a given userName', async() => {
-  const {res, next, mockClear } = getMockRes();
-  mockClear();
-  await getUser(req, res);
-  expect(res.status).toHaveBeenCalledWith(200);
 
-  it('should return an error if there are no params', async() => {
+
+describe('Get user', () => {
+  it('should get a user with a given userName', async () => {
+    const req = getMockReq({
+      params: {
+        userName: testUser.userName
+      }
+    });
+    const { res, next, mockClear } = getMockRes();
+    mockClear();
+    await getUser(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+
+  it('should return an error if there are no params', async () => {
     const req = getMockReq();
-    const {res, next, mockClear } = getMockRes();
+    const { res, next, mockClear } = getMockRes();
     mockClear();
     await getUser(req, res);
     expect(res.status).toHaveBeenCalledWith(400);
-  })
-})
+  });
+});
 
-it('should authenticate the user', async() => {
-  const {res, next, mockClear } = getMockRes();
-  mockClear();
-  await authenticate(req, res);
-  expect(res.status).toHaveBeenCalledWith(200);
 
-  it('should return an error if the userName and or password is incorrect', async() => {
-    const req = getMockReq({params: {userName: "imWrong", password: "iamnotHashed"}});
-    const {res, next, mockClear } = getMockRes();
+
+describe('Authenticate', () => {
+  it('should authenticate the user', async () => {
+    const req = getMockReq({
+      body: {
+        userName: testUser.userName,
+        password: testUser.password
+      }
+    });
+    const { res, next, mockClear } = getMockRes();
+    mockClear();
+    await authenticate(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+
+  it('should return an error if the userName and or password is incorrect', async () => {
+    const req = getMockReq({ params: { userName: "imWrong", password: "iamnotHashed" } });
+    const { res, next, mockClear } = getMockRes();
     mockClear();
     await authenticate(req, res);
     expect(res.status).toHaveBeenCalledWith(400);
-  })
-})
+  });
+});
 
-it('should update the user', async() => {
-  const {res, next, mockClear } = getMockRes();
-  mockClear();
-  await updateUser(req, res);
-  expect(res.status).toHaveBeenCalledWith(200);
 
-  it('should return an error if the user or loginToken were not given/incorrect', async() => {
+
+describe('Update user', () => {
+  it('should update the user', async () => {
+    const req = getMockReq({
+      body: {
+        loginToken: testUserInDB.loginToken,
+        user: testUser
+      }
+    });
+    const { res, next, mockClear } = getMockRes();
+    mockClear();
+    await updateUser(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+
+  it('should return an error if the user or loginToken were not given/incorrect', async () => {
     const req = getMockReq();
-    const {res, next, mockClear } = getMockRes();
+    const { res, next, mockClear } = getMockRes();
     mockClear();
     await updateUser(req, res);
     expect(res.status).toHaveBeenCalledWith(400);
-  })
-})
+  });
+});
 
-it('should delete the user', async() => {
-  const {res, next, mockClear } = getMockRes();
-  mockClear();
-  await deleteUser(req, res);
-  expect(res.status).toHaveBeenCalledWith(200);
 
-  it('should return an error if the userName and or password is incorrect', async() => {
+
+describe('Delete user', () => {
+  it('should delete the user', async () => {
+    const req = getMockReq({
+      params: {
+        loginToken: testUserInDB.loginToken
+      }
+    });
+    const { res, next, mockClear } = getMockRes();
+    mockClear();
+    await deleteUser(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+
+  it('should return an error if the userName and or password is incorrect', async () => {
     const req = getMockReq();
-    const {res, next, mockClear } = getMockRes();
+    const { res, next, mockClear } = getMockRes();
     mockClear();
     await deleteUser(req, res);
     expect(res.status).toHaveBeenCalledWith(400);
-  })
-})
+  });
+});
